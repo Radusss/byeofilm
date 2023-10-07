a2f6ee647c4f008cf8b1e91472cd01215368db68
Radusss <63651897+Radusss@users.noreply.github.com>
2023-10-07 22:17:03 +0200

    final

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,89 @@
-import { Image, useColorScheme, Pressable } from "react-native"; // Import Image from react-native
-import { Link, Tabs } from "expo-router";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
+// import { Image, useColorScheme, Pressable } from "react-native"; // Import Image from react-native
+// import { Link, Tabs } from "expo-router";
+// import FontAwesome from "@expo/vector-icons/FontAwesome";
 
+// import Colors from "../../constants/Colors";
+
+// export default function TabLayout() {
+//   const colorScheme = useColorScheme();
+
+//   return (
+//     <Tabs
+//       screenOptions={{
+//         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+//       }}
+//     >
+//       <Tabs.Screen
+//         name="index"
+//         options={{
+//           title: "",
+//           tabBarIcon: (
+//             { color, focused } // passing the focused prop to tabBarIcon
+//           ) => (
+//             <Image
+//               source={require("../../assets/images/home.png")} // Update source to your image file for the first tab
+//               style={{
+//                 width: 28,
+//                 height: 28,
+//                 // tintColor: focused
+//                 //   ? Colors[colorScheme ?? "light"].tint
+//                 //   : color, // Apply tint color based on tab focused state
+//               }}
+//             />
+//           ),
+//           headerRight: () => (
+//             <Link href="/modal" asChild>
+//               <Pressable>
+//                 {({ pressed }) => (
+//                   <FontAwesome
+//                     name="info-circle"
+//                     size={25}
+//                     color={Colors[colorScheme ?? "light"].text}
+//                     style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+//                   />
+//                 )}
+//               </Pressable>
+//             </Link>
+//           ),
+//         }}
+//       />
+//       <Tabs.Screen
+//         name="two"
+//         options={{
+//           title: "",
+//           tabBarIcon: (
+//             { color, focused } // passing the focused prop to tabBarIcon
+//           ) => (
+//             <Image
+//               source={require("../../assets/images/analytics.png")}
+//               style={{
+//                 width: 28,
+//                 height: 28,
+//                 // tintColor: focused
+//                 //   ? Colors[colorScheme ?? "light"].tint
+//                 //   : color, // Apply tint color based on tab focused state
+//               }}
+//             />
+//           ),
+//         }}
+//       />
+//     </Tabs>
+//   );
+// }
+import { Image, useColorScheme, Pressable, Linking } from "react-native";
+import { Tabs } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 import Colors from "../../constants/Colors";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+
+  const openWebURL = () => {
+    const url = "https://2023.igem.wiki/groningen/index.html";
+    Linking.openURL(url).catch((err) =>
+      console.error("Couldn't load page", err)
+    );
+  };
 
   return (
     <Tabs
@@ -21,29 +99,26 @@ export default function TabLayout() {
             { color, focused } // passing the focused prop to tabBarIcon
           ) => (
             <Image
-              source={require("../../assets/images/home.png")} // Update source to your image file for the first tab
+              source={require("../../assets/images/home.png")}
               style={{
                 width: 28,
                 height: 28,
-                // tintColor: focused
-                //   ? Colors[colorScheme ?? "light"].tint
-                //   : color, // Apply tint color based on tab focused state
+                // You can utilize the tintColor code if you want a visual change when a tab is focused
+                // tintColor: focused ? Colors[colorScheme ?? "light"].tint : color,
               }}
             />
           ),
           headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? "light"].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
+            <Pressable onPress={openWebURL}>
+              {({ pressed }) => (
+                <FontAwesome
+                  name="info-circle"
+                  size={25}
+                  color={Colors[colorScheme ?? "light"].text}
+                  style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                />
+              )}
+            </Pressable>
           ),
         }}
       />
@@ -51,17 +126,13 @@ export default function TabLayout() {
         name="two"
         options={{
           title: "",
-          tabBarIcon: (
-            { color, focused } // passing the focused prop to tabBarIcon
-          ) => (
+          tabBarIcon: ({ color, focused }) => (
             <Image
               source={require("../../assets/images/analytics.png")}
               style={{
                 width: 28,
                 height: 28,
-                // tintColor: focused
-                //   ? Colors[colorScheme ?? "light"].tint
-                //   : color, // Apply tint color based on tab focused state
+                // tintColor: focused ? Colors[colorScheme ?? "light"].tint : color,
               }}
             />
           ),
--- app/modal.tsx
+++ app/modal.tsx
@@ -1,18 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { Platform, StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+// import EditScreenInfo from '../components/EditScreenInfo';
+// import { Text, View } from '../components/Themed';
+
+// export default function ModalScreen() {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>Modal</Text>
+//       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+//       <EditScreenInfo path="app/modal.tsx" />
+
+//       {/* Use a light status bar on iOS to account for the black space above the modal */}
+//       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   title: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//   },
+//   separator: {
+//     marginVertical: 30,
+//     height: 1,
+//     width: '80%',
+//   },
+// });
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Linking,
+  Platform,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 export default function ModalScreen() {
+  const handlePress = () => {
+    // Specify your web URL here
+    const url = "https://www.example.com";
+
+    Linking.openURL(url).catch((err) =>
+      console.error("An error occurred", err)
+    );
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/modal.tsx" />
+      <View style={styles.separator} />
+
+      <TouchableOpacity onPress={handlePress} style={styles.linkContainer}>
+        <Text style={styles.linkText}>Open Web URL</Text>
+      </TouchableOpacity>
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
   );
 }
@@ -20,16 +72,25 @@ export default function ModalScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
+  },
+  linkContainer: {
+    padding: 10,
+    backgroundColor: "#4285f4",
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  linkText: {
+    color: "#fff",
   },
 });
